4bddf2489d774c9eae8f13f4f48549db5778151f
Heitor Pires <heitorfernandes_3@hotmail.com>
2021-10-08 19:36:52 -0300

    create ApplicationApp

---
+++ ApplicationApp/Interfaces/Generics/InterfaceGenericApp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ApplicationApp.Interfaces.Generics
+{
+    public interface InterfaceGenericApp<T> where T : class
+    {
+        Task Add(T entity);
+
+        Task Update(T entity);
+
+        Task Delete(T entity);
+
+        Task<T> GetEntityById(int id);
+
+        Task<List<T>> List();
+
+    }
+}
---
+++ ApplicationApp/OpenApp/AppProduct.cs
@@ -0,0 +1,42 @@
+﻿using ApplicationApp.Interfaces;
+using Domain.Interfaces.InterfaceProduct;
+using Entities.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ApplicationApp.OpenApp
+{
+    public class AppProduct : InterfaceProductApp
+    {
+        IProduct _IProduct;
+        public AppProduct(IProduct IProduct)
+        {
+            _IProduct = IProduct;
+        }
+
+        public async Task Add(Product entity)
+        {
+            await _IProduct.Add(entity);
+        }
+
+        public async Task Delete(Product entity)
+        {
+            await _IProduct.Delete(entity);
+        }
+
+        public async Task<Product> GetEntityById(int id)
+        {
+           return await _IProduct.GetById(id);
+        }
+
+        public async Task<List<Product>> List()
+        {
+            return await _IProduct.List();
+        }
+
+        public async Task Update(Product entity)
+        {
+            await _IProduct.Update(entity);
+        }
+    }
+}
